a8d9208c410c4c6c6d098a8341f99eddbe599ba5
Gabe Fernando <gef756@gmail.com>
2015-07-23 08:12:40 -0400

    ENH: basic control structures 1

--- src/clj_prj03/core.clj
+++ src/clj_prj03/core.clj
@@ -8,4 +8,49 @@
 
 (println "Cleanliness is next to godliness")
 
-(defn train [] (println "Choo choo!"))
+(defn train [] (println "Choo choo!o"))
+
+(+ 1 (* 2 3) 4)
+
+
+; Control structures
+
+(println (if true
+            "abra cadabra"
+            "hocus pocus"))
+
+(if true
+  (do (println "Success!")
+      "abra cadabra")
+  (do (println "Failure:(")
+      "hocus pocus"))
+
+(when true
+   (println "Success!")
+   "abra cadabra")
+
+(def failed-protagonist-names
+    ["Larry Potter"
+     "Doreen the Explorer"
+     "The Incredible Bulk"])
+
+; Equality testing
+(println (nil? 1))
+(println (nil? nil))
+(= 1 1)
+(= nil nil)
+(= 1 2)
+
+; Numbers
+93
+1.2
+1/5
+
+; String examples
+"Lord Voldemort"
+"\"He who must not be named\""
+"\"Great cow of Moscow!\" - Hermes Conrad"
+
+; String interpolation
+(def name "Chewbacca")
+(println (str "\"Rawwwwwwwwrrrrr\" - " name))
