3b646d33b2262f69b5317be5a8c781c1d67915c7
Gabe Fernando <gef756@gmail.com>
2015-10-05 19:44:12 -0400

    ENH: chapter 6, part 2: working with immutable data structures

--- src/clj_prj03/ch6_functional.clj
+++ src/clj_prj03/ch6_functional.clj
@@ -39,3 +39,52 @@
 
 ;; Section 1.2: Side Effects
 ; Clojure core data structures are all immutable!
+
+;;; Part 2: Immutable Data structures
+
+;; 2.1 Recursion instead of for/while
+; The functional alternative to mutation is recursion
+
+(defn no-mutation
+  [x]
+  (println x) ; --> original x
+
+  ;; let creates a new scope
+  (let [x "Kafka Human"]
+    (println x)) ; --> Kafka Human
+
+  ;; Exiting the let scope, x is the same
+  (println x)) ; --> original x
+(no-mutation "Existential Angst Person")
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals accum]
+    (if (empty? vals)
+      accum
+      (sum (rest vals) (+ (first vals) accum)))))
+
+; evaluation is as follows:
+(sum [39 5 1])
+(sum [39 5 1] 0)
+(sum [5 1] 39)
+(sum [1] 44)
+(sum [] 45)
+
+; Use recur!
+; wait?!?!?! no tail-call optimization????
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals accum]
+    (if (empty? vals)
+      accum
+      (recur (rest vals) (+ (first vals) accum)))))
+
+;; 2.2 Functional Composition instead of Attribute mutation
+
+(require '[clojure.string :as s])
+(defn clean
+  [text]
+  (s/replace (s/trim text) #"lol" "LOL"))
+(clean "My boa constrictor is so sassy lol!")
